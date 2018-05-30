6e401d0a4c373554ac14f5859af9a419aa877591
Khaled Alhendi <khaled.alhendi@its.ws>
2018-05-30 11:54:42 +0300

    Add api models, entities, and controllers.

---
+++ NG-Task/Entities/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ng_api.Entities
+{
+    public class AccountType
+    {
+        [Key]
+        [StringLength(2)]
+        public string Type { set; get; } 
+    }
+}
