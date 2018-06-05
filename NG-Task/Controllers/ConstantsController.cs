d530cd45dc459b8df2ceaab67bb340a0b339612c
Khaled Alhendi <khaled.alhendi@its.ws>
2018-06-05 15:22:30 +0300

    Added api calls for accountType and currency

--- NG-Task/Controllers/ConstantsController.cs
+++ NG-Task/Controllers/ConstantsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NG_Task.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,24 @@ namespace NG_Task.Controllers
     [Produces("application/json")]
     [Route("api/const")]
     public class ConstantsController : Controller
-    { 
+    {
+        private NGContext NGContext;
+
+        public ConstantsController(NGContext NGContext)
+        {
+            this.NGContext = NGContext;
+        }
+
         [HttpGet("accountType")]
         public IActionResult GetAccountTypes()
         {
-            return Ok(new []{ "CK","SV","KS" }); 
+            return Ok(NGContext.AccountTypes.Select(at => at.Type).ToArray()); 
+        }
+
+        [HttpGet("currency")]
+        public IActionResult GetCurrencies()
+        {
+            return Ok(NGContext.Currencies.Select(c => c.ISO).ToArray());
         }
 
         [HttpGet("classCode")]
