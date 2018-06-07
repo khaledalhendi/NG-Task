0d1721dcceef20ebfb7bbc45a0d51f40a7edd0ff
OmarDevT <alothman495@yahoo.com>
2018-06-07 15:47:33 +0300

    added a UI search bar

--- NG-Task/ClientApp/components/CustomerList.tsx
+++ NG-Task/ClientApp/components/CustomerList.tsx
@@ -5,6 +5,7 @@ import { ApplicationState } from '../store';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { CustomerSummary } from "../store/Customer";
+import Search from 'react-search-box';
 
 
 interface CustomerListPros
@@ -13,9 +14,33 @@ interface CustomerListPros
 }
 
 export class CustomerList extends React.Component<CustomerListPros, {}>{
+    handleChange(value) {
+        console.log(value);
+    }
+    renderNgSearchBar() {
+        return (<div className="app">
+            <div className="app__content">
+                <div className="content__search content__search--with-full_name">
+                
+                    <div className="search__component">
+                        <Search
+                            data={this.props.customers}
+                            onChange={this.handleChange.bind(this)}
+                            placeholder="Search By Customer RIM/Name"
+                            class="search-class"
+                            searchKey="name"
+                        />
+                    
+                    </div>
+                </div>
+            </div>
+        </div>);
+    }
 
     render() {
+       
         return <div>
+            {this.renderNgSearchBar()}
             Customers: {this.props.customers.length}
             <ul>
                 {this.props.customers.map(c =>
