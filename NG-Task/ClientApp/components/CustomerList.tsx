92aad7807601e28d0b41def0a35a8981ce9991c9
OmarDevT <alothman495@yahoo.com>
2018-06-19 13:04:48 +0300

    added state to CustomerList

--- NG-Task/ClientApp/components/CustomerList.tsx
+++ NG-Task/ClientApp/components/CustomerList.tsx
@@ -13,24 +13,42 @@ interface CustomerListPros
     customers: CustomerSummary[]; 
 }
 
-export class CustomerList extends React.Component<CustomerListPros, {}>{
+interface CustomerListState {
+    filtered: CustomerSummary[];
+}
+export class CustomerList extends React.Component<CustomerListPros, CustomerListState>{
     handleChange(value) {
-        console.log(value);
+        //console.log(typeof value)
+        //console.log(value);
+    }
+
+    componentWillMount() {
+        this.setState({ filtered: this.props.customers });
+        this.filteredCustomers("A");
+    }
+    filteredCustomers(filterInput: string) {
+        //let filtered: CustomerSummary[] = [{ name: filterInput, id: 0, branch: "b1" }, { name: "2", id: 1, branch:"b2" }];
+        let filtered: CustomerSummary[] = this.props.customers;
+        //do filter logic 
+        this.setState({ filtered }); //this means filterd global = local 
     }
     renderNgSearchBar() {
+        var inBox = this.handleChange
+        //console.log(inBox)
         return (<div className="app">
             <div className="app__content">
                 <div className="content__search content__search--with-full_name">
                 
                     <div className="search__component">
+                      
                         <Search
                             data={this.props.customers}
                             onChange={this.handleChange.bind(this)}
                             placeholder="Search By Customer RIM/Name"
                             class="search-class"
                             searchKey="name"
-                        />
-                    
+                    />
+                
                     </div>
                 </div>
             </div>
@@ -41,9 +59,9 @@ export class CustomerList extends React.Component<CustomerListPros, {}>{
        
         return <div>
             {this.renderNgSearchBar()}
-            Customers: {this.props.customers.length}
+            Customers: {this.state.filtered.length}
             <ul>
-                {this.props.customers.map(c =>
+                {this.state.filtered.map(c =>
                     <li key={c.id}>
                         <Link to={`/${c.id}`}>{c.name}</Link>
                     </li>
