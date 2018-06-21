b07853f229c2fab0265657538e6894f7dd2df130
Khaled Alhendi <khaled.alhendi@its.ws>
2018-06-21 14:48:49 +0300

    Added form input validation including error and success messages.

--- NG-Task/ClientApp/components/CreateAccountForm.tsx
+++ NG-Task/ClientApp/components/CreateAccountForm.tsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from "redux";
 import { connect } from 'react-redux';
 import { CreateCustomerAccount } from "../store/Customer";
 import { findDOMNode } from 'react-dom';
-import { Form, FormGroup, ControlLabel, InputGroup, FormControl, Col, DropdownButton, MenuItem, PageHeader } from 'react-bootstrap';
+import { Form, FormGroup, ControlLabel, InputGroup, FormControl, Col, DropdownButton, MenuItem, PageHeader, Alert } from 'react-bootstrap';
 import configureStore from '../configureStore';
 
 type CreateAccountProp =
@@ -22,17 +22,23 @@ interface CreateAccountFormState
     selectedClassCode?: string,
     selectedCurrencyISO?: string,
     balanceInput?: string, 
+
+    formMessage?: string, 
+    formMessageStyle?: string, 
+
+    isAccountTypeInvalid?: boolean, 
+    isClassCodeInvalid?: boolean, 
+    isBalanceInvalid?: boolean, 
 }
 
 export class CreateAccountForm extends React.Component<CreateAccountProp, CreateAccountFormState>{
 
-    constructor(props: CreateAccountProp)
-    {
-        super(props); 
+    constructor(props: CreateAccountProp) {
+        super(props);
+
     }
 
-    componentWillMount() 
-    {
+    componentWillMount() {
         this.AppendState(
             {
                 balanceInput: null,
@@ -44,7 +50,7 @@ export class CreateAccountForm extends React.Component<CreateAccountProp, Create
 
     componentWillReceiveProps(nextProps: CreateAccountProp) {
         if (nextProps.ShouldReset && nextProps.ShouldReset != this.props.ShouldReset) {
-            this.resetForm(); 
+            this.resetForm();
         }
     }
 
@@ -53,68 +59,87 @@ export class CreateAccountForm extends React.Component<CreateAccountProp, Create
             <PageHeader>
                 <small>Create Account</small>
             </PageHeader>
+            {this.state.formMessage ? this.showFormAlert(this.state.formMessageStyle, this.state.formMessage) : null}
+
             <Form className="panel" onSubmit={e => e.preventDefault()}>
-            <FormGroup className="panel">
-            <ControlLabel>AccountType</ControlLabel>
-                <select className="form-control" value={this.state.selectedAccountType != null ? this.state.selectedAccountType : ''}
-                    onChange={this.OnAccountTypeChangeHandler}>
-                {this.state.selectedAccountType == null ? <option value=''></option> : ''}
-                {this.renderOptions(this.props.accountTypes)}
-                </select>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>
-                Class Code
-            </ControlLabel>
-                <select className="form-control" value={this.state.selectedClassCode != null ? this.state.selectedClassCode : ""}
-                    onChange={this.OnClassCodeChangeHandler}>
-                {this.state.selectedClassCode == null ? <option value=''></option> : ''}
-                {this.renderOptions(this.props.classCodes)}
-                </select>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Balance</ControlLabel>
-                 <InputGroup>
-                    <InputGroup.Addon>
-                        $
-                    </InputGroup.Addon>
-                    <input className="form-control" type="text" value={this.state.balanceInput ? this.state.balanceInput : ""}
-                        placeholder="balance" pattern="[1-9]\\d*(\\\.\\d{0,3})?"
-                        onInput={this.BalanceChangeHandler}
-                        onKeyPress={this.OnBalanceSubmitHandler}
+                <FormGroup className="panel" validationState={this.state.isAccountTypeInvalid ? "error" : null}>
+                    <ControlLabel>AccountType</ControlLabel>
+                    <select className="form-control" value={this.state.selectedAccountType != null ? this.state.selectedAccountType : ''}
+                        onChange={this.OnAccountTypeChangeHandler}>
+                        {this.state.selectedAccountType == null ? <option value=''></option> : ''}
+                        {this.renderOptions(this.props.accountTypes)}
+                    </select>
+                </FormGroup>
+                <FormGroup validationState={this.state.isClassCodeInvalid ? "error" : null}>
+                    <ControlLabel>Class Code</ControlLabel>
+                    <select className="form-control" value={this.state.selectedClassCode != null ? this.state.selectedClassCode : ""}
+                        onChange={this.OnClassCodeChangeHandler}>
+                        {this.state.selectedClassCode == null ? <option value=''></option> : ''}
+                        {this.renderOptions(this.props.classCodes)}
+                    </select>
+                </FormGroup>
+                <FormGroup validationState={this.state.isBalanceInvalid ? "error" : null}>
+                    <ControlLabel>Balance</ControlLabel>
+                    <InputGroup>
+                        <InputGroup.Addon>$</InputGroup.Addon>
+                        <input className="form-control" type="text" value={this.state.balanceInput ? this.state.balanceInput : ""}
+                            placeholder="balance" pattern="[1-9]\\d*(\\\.\\d{0,3})?"
+                            onInput={this.BalanceChangeHandler}
+                            onKeyPress={this.OnBalanceSubmitHandler}
                         />
-                    <InputGroup.Addon>
-                        <select className=""
-                            value={this.state.selectedCurrencyISO ? this.state.selectedCurrencyISO : "Select"}
-                            onChange={this.OnCurrencyChangeHandler}>
-                            {this.state.selectedCurrencyISO == null ? <option value=''></option> : ''}
-                            {this.renderOptions(this.props.currencies)}
-                        </select>
-                    </InputGroup.Addon>
-                </InputGroup>
-            </FormGroup>
-            <div>
-                <button className="btn btn-success center-block" onClick={this.CreateClickHandler}>Create</button>
-            </div>
+                        <InputGroup.Addon>
+                            <select className=""
+                                value={this.state.selectedCurrencyISO ? this.state.selectedCurrencyISO : "Select"}
+                                onChange={this.OnCurrencyChangeHandler}>
+                                {this.state.selectedCurrencyISO == null ? <option value=''></option> : ''}
+                                {this.renderOptions(this.props.currencies)}
+                            </select>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div>
+                    <button className="btn btn-success center-block" onClick={this.CreateClickHandler}>Create</button>
+                </div>
             </Form>
         </div>;
     };
 
-    renderOptions(options: string[])
-    {
+    renderOptions(options: string[]) {
         return (options.map(o =>
             <option key={o} value={o}>{o}</option>
         ));
     }
 
+    showFormAlert(style: string, message: string) {
+        return <Alert bsStyle={style} onDismiss={this.handleDismissMessage}>
+            <p>
+                {message}
+            </p>
+        </Alert>;
+    }
+
     private resetForm() {
-        this.setState({ selectedAccountType: null, selectedClassCode: null, selectedCurrencyISO: null, balanceInput: null });
+        this.setState({
+            selectedAccountType: null,
+            selectedClassCode: null,
+            selectedCurrencyISO: null,
+            balanceInput: null,
+            formMessage: null,
+            isAccountTypeValid: null,
+            isbalanceValid: null,
+            isClassCodeValid: null,
+            isCurrencyISOValid: null,
+        } as CreateAccountFormState);
     }
 
     //helper function
-    private AppendState(newState: CreateAccountFormState)
+    private AppendState(newState: CreateAccountFormState) {
+        this.setState((p) =>  { return { ...p, ...newState }});
+    }
+
+    handleDismissMessage = () => 
     {
-        this.setState({ ...this.state, ...newState });
+        this.AppendState({formMessage:null});
     }
 
     OnBalanceSubmitHandler = (target) => {
@@ -130,19 +155,23 @@ export class CreateAccountForm extends React.Component<CreateAccountProp, Create
         //populate classCodes
         this.props.AccountTypeSelected(accountType); 
 
-        this.AppendState({ selectedAccountType: accountType, selectedClassCode: null });
+        this.AppendState({
+            selectedAccountType: accountType, isAccountTypeInvalid: null, 
+            selectedClassCode: null, isClassCodeInvalid: null
+        });
     }
 
     OnClassCodeChangeHandler = (event: React.FormEvent<HTMLSelectElement>) => {
 
         let classCode = event.currentTarget.value;
-        this.AppendState({ selectedClassCode: classCode });
+        this.AppendState({ selectedClassCode: classCode, isClassCodeInvalid: null });
     }
 
     OnCurrencyChangeHandler = (event: React.FormEvent<HTMLSelectElement>) => {
 
         let currencyISO = event.currentTarget.value;
-        this.AppendState({ selectedCurrencyISO: currencyISO });
+
+        this.AppendState({ selectedCurrencyISO: currencyISO, isBalanceInvalid: null });
     }
 
     BalanceChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
@@ -151,24 +180,53 @@ export class CreateAccountForm extends React.Component<CreateAccountProp, Create
 
         const balanceInput = (input.validity.valid) ? input.value : (this.state.balanceInput);
 
-        this.AppendState({ balanceInput });
+        this.AppendState({ balanceInput, isBalanceInvalid: null  });
 
         return false; 
     }
 
     CreateClickHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+        let anyError = false; 
 
-        //validate input here bro 
-        let account: CreateCustomerAccount = 
+        if (!this.state.selectedAccountType) {
+            this.AppendState({ isAccountTypeInvalid: true }); 
+            anyError = true;  
+        }
+
+        if (!this.state.selectedClassCode) {
+            this.AppendState({ isClassCodeInvalid: true });
+            anyError = true;
+        }
+
+        if (!this.state.selectedCurrencyISO || !(this.state.balanceInput)) {
+            this.AppendState({ isBalanceInvalid: true});
+            anyError = true;
+        }
+
+        if (anyError === true) {
+            this.AppendState({
+                formMessage: "Please be sure to set enter all required form data.", 
+                formMessageStyle: "danger"
+            });
+            return; 
+        }
+        
+        let account: CreateCustomerAccount =
             {
-                accountType: this.state.selectedAccountType, 
-                classCode: this.state.selectedClassCode, 
-                currencyISO: this.state.selectedCurrencyISO, 
+                accountType: this.state.selectedAccountType,
+                classCode: this.state.selectedClassCode,
+                currencyISO: this.state.selectedCurrencyISO,
                 balance: +this.state.balanceInput,
-                customerId: null
-            }; 
+                customerId: null //form doesn't know customer id. 
+            };
 
         this.props.AddAccount(account);
-        this.resetForm(); 
+        this.resetForm();
+
+        this.AppendState({
+            formMessage: "Account was created!",
+            formMessageStyle: "success"
+        });
+        
     }
 }
