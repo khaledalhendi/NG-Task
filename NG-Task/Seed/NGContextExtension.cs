94aebad0701414597ae82e737ab1d12b426c4ed5
Khaled Alhendi <khaled.alhendi@its.ws>
2018-06-25 12:36:26 +0300

    Cleaned up seeding methods .

--- NG-Task/Seed/NGContextExtension.cs
+++ NG-Task/Seed/NGContextExtension.cs
@@ -18,30 +18,27 @@ namespace NG_Task.Seed
             context.Accounts.RemoveRange(context.Accounts);
 
             context.SaveChanges();
-            
-            //seed lookup tables
-            SeedAccountTypes(context);
-            context.SaveChanges();
-            SeedClassCodes(context);
-            SeedCurrencies(context);
 
-            context.SaveChanges();
-
-            //seed customers tables
-            SeedCustomers(context);
-            context.SaveChanges();
-            
-            //seed accounts tables (depends on customer
-            SeedAcconuts(context);
-            context.SaveChanges();
-        }
-      
-        #region AccountType
-        private static void SeedAccountTypes(NGContext context)
-        {
+            //generate data
             IEnumerable<AccountType> AccountTypes = CreateAccountTypes();
-
             context.AccountTypes.AddRange(AccountTypes);
+            context.SaveChanges(); 
+
+            IEnumerable<ClassCode> ClassCodes = CreateClassCodes(AccountTypes);
+            context.ClassCodes.AddRange(ClassCodes);
+            context.SaveChanges();
+
+            IEnumerable<Currency> Currencies = CreateCurrencies();
+            context.Currencies.AddRange(Currencies);
+            context.SaveChanges();
+
+            IEnumerable<Customer> Customers = CreateCustomers();
+            context.Customers.AddRange(Customers);
+            context.SaveChanges();
+
+            IEnumerable<Account> Accounts = CreateAccounts(Customers, AccountTypes.ToArray(), ClassCodes.ToArray(), Currencies.ToArray());
+            context.Accounts.AddRange(Accounts);
+            context.SaveChanges();
         }
 
         private static IEnumerable<AccountType> CreateAccountTypes()
@@ -53,16 +50,6 @@ namespace NG_Task.Seed
                 new AccountType{Type = "CD" },
             };
         }
-        #endregion
-        #region ClassCodes
-        private static void SeedClassCodes(NGContext context)
-        {
-            IEnumerable<ClassCode> ClassCodes = CreateClassCodes(context.AccountTypes.ToList());
-
-            context.ClassCodes.AddRange(ClassCodes);
-            
-        }
-
         private static IEnumerable<ClassCode> CreateClassCodes(IEnumerable<AccountType> accountTypes)
         {
             const int seed = 42;
@@ -86,92 +73,6 @@ namespace NG_Task.Seed
             }
             return classes;
         }
-        #endregion
-        #region Customer
-        private static void SeedCustomers(NGContext context)
-        {
-            IEnumerable<Customer> Customers = CreateCustomers();
-
-            context.Customers.AddRange(Customers);
-            
-        }
-
-        private static IEnumerable<Customer> CreateCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer(){
-                Name = "Abdullah",
-                OpenDate = new DateTime(1990,7,5),
-                Branch = "Banking"
-                },
-            new Customer(){
-                Name = "Ahmad",
-                OpenDate = new DateTime(2000,8,10),
-                Branch = "Medical"
-                },
-            new Customer(){
-                Name = "Sherif",
-                OpenDate = new DateTime(2090,5,9),
-                Branch = "Business"
-                },
-            new Customer(){
-                Name = "Yusri",
-                OpenDate = new DateTime(2015,1,6),
-                Branch = "Banking"
-                }
-            };
-        }
-
-        #endregion
-        #region Accounts
-        private static void SeedAcconuts(NGContext context)
-        {
-            IEnumerable<Account> Accounts = CreateAccounts(context, context.Customers.ToList());
-
-            context.Accounts.AddRange(Accounts);
-            
-        }
-
-        private static IEnumerable<Account> CreateAccounts(NGContext context, IEnumerable<Customer> customers)
-        {
-            AccountType[] types = context.AccountTypes.Include(a => a.ClassCodes).ToArray();
-
-            Currency[] currencies = context.Currencies.ToArray(); 
-
-            const int seed = 42;
-            Random r = new Random(seed); //reusable
-
-            List<Account> accounts = new List<Account>();
-            foreach (var c in customers)
-            {
-                for (int i = 0; i < 1 + r.Next(5); i++)
-                {
-                    AccountType accountType = types[r.Next(3)];
-                    ClassCode[] classCodes = accountType.ClassCodes.ToArray(); 
-
-                    accounts.Add(new Account()
-                    {
-                        CustomerId = c.Id,
-                        Balance = (decimal)((2000 * r.Next(1,5)) + (1000 * r.NextDouble()) ),
-                        CurrencyISO = currencies[r.Next(currencies.Length)].ISO,
-                        AccountType = accountType.Type,
-                        ClassCode = classCodes[r.Next(classCodes.Length)].Code,
-                    });
-                }
-            }
-            return accounts;
-
-        }
-        #endregion
-        #region Currency
-        private static void SeedCurrencies(NGContext context)
-        {
-            IEnumerable<Currency> Currencys = CreateCurrencies();
-
-            context.Currencies.AddRange(Currencys);
-        }
-
         private static IEnumerable<Currency> CreateCurrencies()
         {
             return new Currency[]
@@ -209,6 +110,57 @@ namespace NG_Task.Seed
             };
 
         }
-        #endregion
+        private static IEnumerable<Customer> CreateCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer(){
+                Name = "Abdullah",
+                OpenDate = new DateTime(1990,7,5),
+                Branch = "Banking"
+                },
+            new Customer(){
+                Name = "Ahmad",
+                OpenDate = new DateTime(2000,8,10),
+                Branch = "Medical"
+                },
+            new Customer(){
+                Name = "Sherif",
+                OpenDate = new DateTime(2090,5,9),
+                Branch = "Business"
+                },
+            new Customer(){
+                Name = "Yusri",
+                OpenDate = new DateTime(2015,1,6),
+                Branch = "Banking"
+                }
+            };
+        }
+        private static IEnumerable<Account> CreateAccounts(IEnumerable<Customer> customers, AccountType[] types, ClassCode[] classCodes, Currency[] currencies)
+        {
+            const int seed = 42;
+            Random r = new Random(seed); //reusable
+
+            List<Account> accounts = new List<Account>();
+            foreach (var c in customers)
+            {
+                for (int i = 0; i < 1 + r.Next(5); i++)
+                {
+                    AccountType accountType = types[r.Next(3)];
+                    ClassCode classCode = classCodes[r.Next(classCodes.Length)]; 
+
+                    accounts.Add(new Account()
+                    {
+                        CustomerId = c.Id,
+                        Balance = (decimal)((2000 * r.Next(1,5)) + (1000 * r.NextDouble()) ),
+                        CurrencyISO = currencies[r.Next(currencies.Length)].ISO,
+                        AccountType = accountType.Type,
+                        ClassCode = classCode.Code,
+                    });
+                }
+            }
+            return accounts;
+
+        }
     }
 }
